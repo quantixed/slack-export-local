2cca0f03925c70b1b1ca508bdf6be3422199fcc4
Stephen Royle <s.j.royle@warwick.ac.uk>
2024-07-07 12:08:28 +0100

    Fix: export all links

--- slack-export-local.py
+++ slack-export-local.py
@@ -3,7 +3,7 @@ import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
-def download_file(url, root_dir):
+def download_file(url, root_dir, html_file_path):
     """Download a file from a URL into the specified directory structure if it matches allowed domains."""
     allowed_domains = ['files.slack.com', 'avatars.slack-edge.com', 'secure.gravatar.com']
     parsed_url = urlparse(url)
@@ -19,25 +19,33 @@ def download_file(url, root_dir):
     response = requests.get(url)
     with open(file_path, 'wb') as file:
         file.write(response.content)
-    return file_path
+    # Return the relative path to the html file
+    file_path_relative = os.path.relpath(file_path, os.path.dirname(html_file_path))
+    return file_path_relative
 
 def update_html_file(html_file_path, root_dir):
     """Update HTML file to download and reference local copies of external resources."""
     with open(html_file_path, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file, 'html.parser')
-    
-    # Find all img tags and any other tags that might reference external resources
+
+    # Handle img tags with src attributes
     for img_tag in soup.find_all('img'):
-        original_url = img_tag['src']
-        # Download the file and get the new path if it's from an allowed domain
-        new_path = download_file(original_url, root_dir)
-        # Update the src attribute to point to the local file or keep it unchanged
-        img_tag['src'] = new_path
+        original_url = img_tag.get('src')
+        if original_url:
+            new_path = download_file(original_url, root_dir, html_file_path)
+            img_tag['src'] = new_path
+    
+    # Handle tags with href attributes (e.g., a, link)
+    for href_tag in soup.find_all(href=True):
+        original_url = href_tag.get('href')
+        if original_url:
+            new_path = download_file(original_url, root_dir, html_file_path)
+            href_tag['href'] = new_path
     
     # Write the updated HTML back to a file
     with open(html_file_path, 'w', encoding='utf-8') as file:
         file.write(str(soup))
-
+        
 # Example usage
 html_file_path = '/path/to/html_output/channel/announcements/index.html'
 root_dir = '/path/to/html_output/attachments'
